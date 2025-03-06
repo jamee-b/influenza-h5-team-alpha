1051c899f63fa8ca8a7258eaa29b3f48769cba80
cdonel <88918165+cdonel@users.noreply.github.com>
2025-03-06 04:32:20 -0500

    Removed user input and standardized input/output file

--- 3_extract_host_organisms.py
+++ 3_extract_host_organisms.py
@@ -1,35 +1,36 @@
-from Bio import SeqIO
+import utils
 import pandas as pd
 import re
 
 def main():
-    fasta_file = input("Enter fasta file: ")
-    subtype = input("Enter subtype: ")
-    seq_records = read_fasta_to_seq_records(fasta_file)
-    write_host_organisms_csv(seq_records, subtype)
+    for H5 in utils.subtypes:
+        read_path = "data/subtype_sequences/{0}/01_{0}.fasta".format(H5)
+        write_path = "data/subtype_sequences/{0}/{0}_hosts.csv".format(H5)
+        seq_records = utils.read_fasta(read_path)
+        host_df = get_hosts(seq_records)
+        utils.df_to_csv(host_df, write_path)
 
-# Parse fasta file and read in as sequence record. Append to list.
-def read_fasta_to_seq_records(fasta_file):
-    seq_records = []
-    for record in SeqIO.parse(fasta_file, "fasta"):
-        seq_records.append(record)
-    return seq_records
-
-# Produces dataframe containing unique host organisms with number of sequences per organism.
-def write_host_organisms_csv(seq_records, subtype):
+def get_hosts(seq_records):
     temp_list = []
     for i in range(0, len(seq_records)):
         record = seq_records[i]
-        try:
-            group_1 = re.search(r'A/(.*?)/', record.description).group(1)
-            temp_list.append(group_1)
-        except:
-            print("No host extracted using regex search.")
-    df = pd.DataFrame({'host_organism':list(set(temp_list))})
 
-    for i in range(0, df['host_organism'].size):
-        df.loc[i, 'seq_count'] = int(temp_list.count( df.loc[i, 'host_organism']))
-    df.to_csv(subtype + "_host_organisms.csv", index=False)
+        # Extract host name from sequence description using regular expression.
+        try:
+            host_name = re.search(r'A/(.*?)/', record.description).group(1)
+            temp_list.append(host_name)
+        except:
+            print("No host extracted for {0}.".format(record.name))
+
+    host_df = pd.DataFrame({'host_organism':list(set(temp_list))})
+
+
+    # Count occurence of host names extract to get number of sequences for host.
+    for i in range(0, host_df['host_organism'].size):
+        host_df.loc[i, 'seq_count'] = int(temp_list.count(host_df.loc[i, 'host_organism']))
+    
+    return host_df
+
 
 if __name__ == "__main__":
     main()
