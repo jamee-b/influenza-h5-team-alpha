ca3b726ff842b70ac43dd519b84c23908b0b890a
cdonel <88918165+cdonel@users.noreply.github.com>
2025-03-07 04:40:08 -0500

    Standardized input/output files Moved methods over to utils

--- 9_plot_results.py
+++ 9_plot_results.py
@@ -1,23 +1,24 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
-import pandas as pd
+import utils
 
 def main():
-    csv_input_file = input("Enter CSV results file: ")
-    results = read_csv(csv_input_file)
-    avian_results = results.loc[(results['host'] == 'bovine')]
-    plot_data(avian_results)
+    read_path = 'data/cai_results/cai_results.csv' # read file location
+    cai_results = utils.csv_to_df(read_path) # csv to dataframe
+    for H5 in utils.subtypes:
+        write_path = 'data/plots/{0}.png'.format(H5) # destination of write file
+        results_host = cai_results.loc[cai_results['subtype'] == H5] # select data from dataframe of specific host
+        write_boxplot(results_host, write_path) # Make and save boxplot
 
-def plot_data(dataframe):
-    sns.boxplot(dataframe, x='subtype', y='cai', hue='host')
-    plt.xlabel('Influenza A H5 subtypes')
+# Save boxplot produced to read path
+def write_boxplot(dataframe, path):
+    sns.boxplot(dataframe, x='subtype', y='cai_score', hue='host')
+    plt.xlabel('Influenza A virus (H5)')
     plt.ylabel('Codon adaptation index (CAI)')
-    plt.show()
-
-def read_csv(csv_input_file):
-    path = 'Results/{0}'.format(csv_input_file)
-    df = pd.read_csv(path)
-    return df
+    plt.title('Codon adaptation index for {0} across hosts'.format(dataframe['subtype'].unique()[0]))
+    plt.legend(loc='lower right')
+    plt.savefig(path)
+    plt.clf()
 
 if __name__ == "__main__":
     main()
