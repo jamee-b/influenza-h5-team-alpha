ce491f350cd857966f98d09b8d89e1844ee6fdd6
cdonel <88918165+cdonel@users.noreply.github.com>
2025-03-07 01:59:34 -0500

    Standarized input/output files. Moved methods to utils. Standardizes host name

--- 3_extract_host_organisms.py
+++ 3_extract_host_organisms.py
@@ -5,9 +5,10 @@ import re
 def main():
     for H5 in utils.subtypes:
         read_path = "data/subtype_sequences/{0}/01_{0}.fasta".format(H5) # read file location
-        write_path = "data/subtype_sequences/{0}/{0}_hosts.csv".format(H5) # write file destination
+        write_path = "data/subtype_sequences/H5_hosts/{0}_hosts.csv".format(H5) # write file destination
         seq_records = utils.read_fasta(read_path) # read fasta sequences
         host_df = get_hosts(seq_records) # Extract host name and number of times it occurs into dataframe.
+        # seq_records = standardize_seq_records_hosts(seq_records) # Standardize host names in seq record description
         utils.df_to_csv(host_df, write_path) # Write dataframe to csv
 
 # Get host names and occurences from sequence description and return in dataframe.
@@ -17,9 +18,11 @@ def get_hosts(seq_records):
         record = seq_records[i]
 
         # Extract host name from sequence description using regular expression.
+        # Standardize host name for duck, chicken, swine, bovine, and human in record_description.
         try:
             host_name = re.search(r'A/(.*?)/', record.description).group(1)
             temp_list.append(host_name)
+            
         except:
             print("No host extracted for {0}.".format(record.name))
 
@@ -31,7 +34,6 @@ def get_hosts(seq_records):
         host_df.loc[i, 'seq_count'] = int(temp_list.count(host_df.loc[i, 'host_organism']))
     
     return host_df
-
 
 if __name__ == "__main__":
     main()
