0b6bab6177b9ad0b790d0a33db293ed8bc2fe7d5
cdonel <88918165+cdonel@users.noreply.github.com>
2025-03-07 03:07:43 -0500

    Standardized input/output files. Moved some methods over to utils

--- 7_genbank_to_fasta.py
+++ 7_genbank_to_fasta.py
@@ -1,60 +1,53 @@
-from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
-import pandas as pd
+import utils
+
 
 def main():
-    gb_input_file = input("Enter genbank file: ")
-    csv_input_file = input("Enter csv file: ")
-    host = input("Enter host: ")
-    gb_records = read_genbank(gb_input_file)
-    seq_info = read_csv(csv_input_file)
-    seq_records = build_fasta(gb_records, seq_info)
-    write_fasta(seq_records, host)
+    for host in utils.host_names:
+        read_path_gb = "data/genbank/{0}.gb".format(host) # read file location for genbank
+        read_path_csv = "data/genbank/host_info/{0}.csv".format(host) # read file location for csv
+        write_path = "data/host_sequences/{0}.fasta".format(host) # Write file destination
+        gb_records = utils.read_genbank(read_path_gb) # read genbank
+        seq_info = utils.csv_to_df(read_path_csv) # read csv
+        seq_records = get_genbank_cds(gb_records, seq_info) # List containing sequences retrieve from genbank records
+        utils.write_fasta(seq_records, write_path) # Write fasta file
 
-    
-def read_genbank(filename):
-    gb_records = []
-    path = 'Data/Genbank/{0}'.format(filename)
-    for record in SeqIO.parse(path, "genbank"):
-        gb_records.append(record)
-    return gb_records
+# Takes genbank records, extracts cds, and builds sequence record.
+# Looks for genbank records using CSV file built with 6_get_genbank.py
+# Uses protein_id in seq_info to make sure correct cds is being extracted.
+def get_genbank_cds(gb_records, seq_info):
+    seq_records = [] # Empty list to hold sequence records
 
-def read_csv(filename):
-    path = 'Data/Genbank/{0}'.format(filename)
-    df = pd.read_csv(path)
-    return df
+    for i in range(0, len(gb_records)): # loop through length of gb_records
+        record = gb_records[i] # single genbank record
 
-def build_fasta(gb_records, seq_info):
-    seq_records = []
+        if record.features: # If record attribute is features
 
-    for i in range(0, len(gb_records)):
-        record = gb_records[i]
-        if record.features:
-            for feature in record.features:
-                if feature.type == "CDS":
+            for feature in record.features: # for each feature in genbank record
+
+                if feature.type == "CDS": # If feature type is equal to coding sequence (CDS)
+
                     try:
-                        protein_id = feature.qualifiers['protein_id'][0]
+                        protein_id = feature.qualifiers['protein_id'][0] # parse protein id from genbank record
+
                     except:
                         print("{0} not added to sequence records. Does not have protein id.".format(record.name))
-                    if protein_id in seq_info['protein_id'].values:
+
+                    if protein_id in seq_info['protein_id'].values: # If protein id from genbank record matches protein id in <host>.csv
+
                         try:
-                            seq = feature.location.extract(record).seq
-                            id = record.id
-                            description = record.description
-                            name = record.name
-                            seq_record = SeqRecord(seq=seq, id=id, description=description, name=name)
-                            seq_records.append(seq_record)
+                            seq = feature.location.extract(record).seq # Extract CDS sequence from genbank record
+                            id = record.id # Extract id from genbank record
+                            description = record.description # Extract description from genbank record
+                            name = record.name # Extract name from genbank record
+                            seq_record = SeqRecord(seq=seq, id=id, description=description, name=name) # Build sequence record from extracted data
+                            seq_records.append(seq_record) # Add sequence record to seq_records list
+
                         except:
                             print("{0} was not added to sequence records".format(protein_id))
     
-    return seq_records
+    return seq_records # Return list of sequence records
 
-# Write out sequence records into single fasta file.
-def write_fasta(seq_records, host):
-    path = 'Data/Hosts/{0}_reference_genes.fasta'.format(host)
-    with open(path, "w") as output_handle:
-        for i in seq_records:
-            SeqIO.write(i, output_handle, "fasta")
 
 
 if __name__ == '__main__':
